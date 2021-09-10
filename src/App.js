4e17f79df03efc79aabe7f8e40e0c37621aa79a7
thiago <thiago.heilmann@hotmail.com>
2021-09-10 15:41:32 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [valor, setValor] = useState(0);
+  let [resultado, setResultado] = useState(0);
+  let [custom, setCustom] = useState(0);
+  let [people, setPeople] = useState(0);
+
+  function addPeople(e) {
+    setPeople(e.target.value);
+    console.log(people);
+  }
+
+  function addCustom(e) {
+    setCustom(e.target.value);
+  }
+
+  function addValor(e) {
+    setValor(e.target.value);
+  }
+
+  function addResultado(valor) {
+    setResultado(valor);
+  }
+
+  function valorCustom() {
+    addResultado((valor * custom) / 100);
+    console.log(custom);
+  }
+
+  let total = valor / people;
+
+  console.log(total);
+
+  function cinco() {
+    addResultado((total * 5) / 100);
+  }
+  function dez() {
+    addResultado((total * 10) / 100);
+  }
+  function quinze() {
+    addResultado((total * 15) / 100);
+  }
+  function vintecinco() {
+    addResultado((total * 25) / 100);
+  }
+  function cinquenta() {
+    addResultado((total * 50) / 100);
+  }
+
+  function reset() {
+    addResultado(0);
+    total = 0;
+    console.log(total);
+    window.location.reload();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="fundo">
+      <div className="fundo-caixa">
+        <div className="caixa1">
+          <div className="input-valor">
+            <h4>Bill</h4>
+            <input
+              className="input-1"
+              type="number"
+              id="inputvalor"
+              onChange={addValor}
+            ></input>
+          </div>
+          <div>
+            <h4>Select tip %</h4>
+            <div className="botoes">
+              <button onClick={cinco}>5%</button>
+              <button onClick={dez}>10%</button>
+              <button onClick={quinze}>15%</button>
+              <button onClick={vintecinco}>25%</button>
+              <button onClick={cinquenta}>50%</button>
+              <input
+                name="inputvalor"
+                className="input-custom"
+                type="number"
+                placeholder="Custom"
+                onChange={addCustom}
+                onClick={valorCustom}
+              ></input>
+            </div>
+          </div>
+          <div>
+            <h4>Number of people</h4>
+            <input
+              className="input"
+              type="number"
+              placeholder="Number of people"
+              onChange={addPeople}
+            ></input>
+          </div>
+        </div>
+        <div className="caixa2">
+          <div className="totais">
+            <div className="tip-amount">
+              <div id="percent">
+                <h4>Tip Amount</h4>
+                <small>/ person</small>
+              </div>
+              <div>
+                <h1 id="total-tip">R${resultado}</h1>
+              </div>
+            </div>
+            <div className="total">
+              <div>
+                <h4>Total</h4>
+                <small>/ person</small>
+              </div>
+              <div>
+                <h1 id="total-tip-person">R${total + resultado}</h1>
+              </div>
+            </div>
+          </div>
+          <button onClick={reset}>RESET</button>
+        </div>
+      </div>
     </div>
   );
 }
